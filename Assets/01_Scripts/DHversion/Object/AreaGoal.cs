0d856487f4e3da51ee44a6b362d8e662cf972d24
kjp2981 <81199996+kjp2981@users.noreply.github.com>
2026-05-26 22:42:29 +0900

    0526_1_D3_A2_대현길 버그 수정

--- Assets/01_Scripts/DHversion/Object/AreaGoal.cs
+++ Assets/01_Scripts/DHversion/Object/AreaGoal.cs
@@ -4,9 +4,12 @@ public class AreaGoal : AreaObject<Area>
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.CompareTag("Player"))
+        if (collision.CompareTag("Player"))
         {
-            ParentArea.Obstacle?.SetActive(false);
+            if (ParentArea.Obstacle != null)
+            {
+                ParentArea.Obstacle.SetActive(false);
+            }
             this.gameObject.SetActive(false);
         }
     }
